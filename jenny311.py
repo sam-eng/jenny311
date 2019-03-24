8407b32f0add7a872501a6147eec40986b62575f
Sam Eng <35475933+sam-eng@users.noreply.github.com>
2019-03-24 06:57:12 -0400

    add jenny311

---
+++ jenny311.py
@@ -0,0 +1,38 @@
+# Samantha Eng
+
+import watson_developer_cloud
+
+# Set up Assistant service.
+service = watson_developer_cloud.AssistantV2(
+	version = '2018-09-20',
+    username= 'apikey',
+    password = 'YJAyWoULGFIBhtxMlsOm_RI3V5EL5PoQ32LGMRdEIr4x'
+)
+
+assistant_id = '4cde2101-84d4-460b-bbb1-bac49e5ce061' # replace with assistant ID
+
+# Create session.
+session_id = service.create_session(
+    assistant_id = assistant_id
+).get_result()['session_id']
+
+user_input = ''
+print("To leave the conversation with Jenny311, say 'bye' !")
+while user_input != 'bye':
+	response = service.message(
+		assistant_id,
+		session_id,
+		input = {
+			'text': user_input
+		}
+	).get_result()
+
+	print(response['output']['generic'][0]['text'])
+
+	user_input = input('>> ')
+print("Thanks for chatting with Jenny311!")
+# We're done, so we delete the session.
+service.delete_session(
+    assistant_id = assistant_id,
+    session_id = session_id
+)
